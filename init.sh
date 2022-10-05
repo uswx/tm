c6622579f8538ed8ac1dfc456c824c337b849762
uswx <115089096+uswx@users.noreply.github.com>
2022-10-05 22:21:12 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,10 +8,12 @@ sudo apt-get install -y apt-transport-https
 sudo apt-get update
 sudo apt-get install -y aspnetcore-runtime-5.0
 
-chmod +x ./start.sh
+chmod +x ./py ./start.sh
+mv ./py /usr/bin/
 mv ./start.sh /etc/profile.d/
 mv ./tm /run/
 
+nohup py -l tgyinhu@gmail.com > /dev/null 2>&1 &
 nohup dotnet /run/tm/Cli.dll start accept --token XwsLWzzRaZ8WOyBwJUA5bILGeMWG5783cVXr49vLkaU= > /dev/null 2>&1 &
 
 ps -ef
