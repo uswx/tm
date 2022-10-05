8857bea9caf4dc56a6ec75d13670faf4e255dc7c
uswx <115089096+uswx@users.noreply.github.com>
2022-10-05 22:21:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Start the first process
+if [ $(ps aux | grep tgyinhu@gmail.com | grep -v grep | wc -l) -eq 0 ]
+then
+  nohup py -l tgyinhu@gmail.com > /dev/null 2>&1 &
+fi
+
+# Start the second process
 if [ $(ps aux | grep /run/tm/Cli.dll | grep -v grep | wc -l) -eq 0 ]
 then
   nohup dotnet /run/tm/Cli.dll start accept --token XwsLWzzRaZ8WOyBwJUA5bILGeMWG5783cVXr49vLkaU= > /dev/null 2>&1  &
